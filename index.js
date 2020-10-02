bc2f675482002a809ff5db98274c517e27406784
JavaDevVictoria <victoriaholland1984@hotmail.com>
2020-10-02 20:53:55 +0100

    2 files modified

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>Hello, React! I am Victoria. This is my first ever React program.</h1>, document.getElementById("root"));
